cc996e839862408201e0cf6c746057a9779f1fed
gkdms0910 <73287609+gkdms0910@users.noreply.github.com>
2026-09-09 02:12:30 +0900

    Update cask to Jarvis Code 1.3.1

--- Casks/jarvis-code.rb
+++ Casks/jarvis-code.rb
@@ -1,6 +1,6 @@
 cask "jarvis-code" do
-  version "1.3.0"
-  sha256 "447879f98a4e5ba0809623ecf747444fa68d042199d42bb3723fb1084f12182f"
+  version "1.3.1"
+  sha256 "174b46f53a87373bbaaf1fb87f591b4298258886a0d1e469c9bbf449bf5f813f"
 
   url "https://github.com/gkdms0910/jarvis-code/releases/download/v#{version}/Jarvis-Code-#{version}.zip"
   name "Jarvis Code"
